87529fd00dc5a52a089b8c37522ba4a33a9caa18
evgenyzaikin <evgenyzaikin96@gmail.com>
2022-01-19 01:54:52 +0300

    Добавлена генерация поля

--- script/main.js
+++ script/main.js
@@ -1,5 +1,8 @@
 "use strict";
 
+
+let game;
+
 //базовое препятствие
 class Barrier {
   constructor(x, y, isFragile, name) {
@@ -138,9 +141,9 @@ class Entity {
   }
 }
 
-class Mushroom extends Entity{
-  constructor(x,y){
-    super(x,y,"Mushroom")
+class Mushroom extends Entity {
+  constructor(x, y) {
+    super(x, y, "Mushroom");
   }
 }
 
@@ -168,7 +171,7 @@ class Game {
     this.field = [];
     this.bombList = [];
     this.wallList = [];
-    this.fragWallList = [];
+    this.boxList = [];
     this.hero = new Hero(0, 0);
     this.mushList = [];
     this.initField(11, 9);
@@ -183,6 +186,97 @@ class Game {
       this.field.push(row);
     }
   }
+  // заполнение игрового поля
+  fillField(numOfMushrooms, numOfBoxes) {
+    // заполнение верхней строчки стенами
+    for (let i = 0; i < game.field[0].lenght; i++) {
+      let barr = new Barrier(i, 0, false, "Wall");
+      game.field[i][0].barrier = barr;
+      this.wallList.push(barr);
+    }
+
+    //заполнение нижней строчки стенами
+    for (let i = 0; i < game.field[0].lenght; i++) {
+      let barr = new Barrier(i, game.field[0].lenght - 1, false, "Wall");
+      game.field[i][game.field[0].lenght - 1].barrier = barr;
+      this.wallList.push(barr);
+    }
+
+    // заполнение левой колонны стенами
+    for (let i = 0; i < game.field.lenght; i++) {
+      let barr = new Barrier(0, i, false, "Wall");
+      game.field[0][i].barrier = barr;
+      this.wallList.push(barr);
+    }
+
+    // заполнение правой колонны стенами
+    for (let i = 0; i < game.field.lenght; i++) {
+      let barr = new Barrier(game.field.lenght - 1, i, false, "Wall");
+      game.field[game.field.lenght - 1][i].barrier = barr;
+      this.wallList.push(barr);
+    }
+
+    // заполнение игрового поля стенами
+    for (let i = 2; i < game.field[0].lenght - 1; i++) {
+      for (let j = 2; j < game.field.lenght - 1; i++) {
+        if (i % 2 == 0 && j % 2 == 0) {
+          let barr = new Barrier(i, j, false, "Wall");
+          game.field[i][j].barrier = barr;
+          this.wallList.push(barr);
+        }
+      }
+    }
+
+    // установка героя
+    this.hero.x = 1;
+    this.hero.y = 1;
+
+    // установка ящиков
+    for (let i = 2; i < game.field[0].lenght - 1; i += 2) {
+      for (let j = 2; j < game.field.lenght - 1; i += 2) {
+        this.setBoxes(i, j, 2);
+      }
+    }
+
+    // установка грибов
+    switch (numOfMushrooms) {
+      case 4:
+        this.setMushroom(5, 5);
+      case 3:
+        this.setMushroom(1, 7);
+      case 2:
+        this.setMushroom(9, 1);
+      case 1:
+        this.setMushroom(9, 7);
+    }
+  }
+  // установка указанного количества ящиков(Box) в зоне 3*3
+  setBoxes(x, y, numOfBoxes) {
+    let dx = 0;
+    let dy = 0;
+    let i = numOfBoxes;
+    let maxTryOfSet = numOfBoxes + 3;
+    while (i) {
+      dx = Math.floor(Math.random() * 3 - 1);
+      dy = Math.floor(Math.random() * 3 - 1);
+      if (game.field[x + dx][y + dy].barrier === null) {
+        let box = new Barrier(x + dx, y + dy, true, "Box");
+        game.field[x + dx][y + dy].barrier = box;
+        game.boxList.push(box);
+        i--;
+      }
+      maxTryOfSet--;
+      if (maxTryOfSet == 0) {
+        break;
+      }
+    }
+  }
+  // добавление гриба
+  setMushroom(x, y) {
+    let mushroom = new Mushroom(x, y);
+    this.mushList.push(mushroom);
+  }
 }
 
-const game = new Game();
+game = new Game();
+game.fillField(2,22);
